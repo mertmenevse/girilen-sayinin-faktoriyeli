ab55a211c605a0f967d2215bf369189cb4dd6f84
Mehmet Mert Menevşe <mert.menevse32@gmail.com>
2021-02-08 18:51:05 +0300

    project pertama

---
+++ faktoriyel.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+
+	int n;
+	int fact=1;
+	
+	printf("Faktoriyelini istedigiz sayiyi, giriniz:");	
+	scanf("%d",&n);
+	
+	
+	while(n!=0) {
+		printf("%d\n",n);
+		fact= fact*n;
+		
+		n--;
+	}
+	printf("%d",fact);	
+	
+
+
+	
+	
+	
+	return 0;
+}
